1e23da9ae47c221e8bd2ee8a225e8ea9dcb2ca59
Dongjiawei <1667504131>
2019-01-18 17:49:52 +0800

    我在bug1分支提交了

--- src/dome/text.java
+++ src/dome/text.java
@@ -3,5 +3,6 @@ package dome;
 public class text {
     public static void main(String[] args) {
         int i=0;
+        int b=1;
     }
 }
---
+++ src/dome/txet2.java
@@ -0,0 +1,5 @@
+package dome;
+
+public class txet2 {
+    int y=0;
+}
